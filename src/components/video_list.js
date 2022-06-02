b2e8cbbf2c64c1a3f95a549f8602ad566adfd53a
BLUE <abhay.sharma@ninehertzindia.com>
2022-06-02 18:28:11 +0530

    6:28

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
     // console.log(props,"here");
     // const videos = props.videos;
+    const videoItems= props.videos.map((video) => {
+        return <VideoListItem key={video.etag} video={video}/>
+    });
+
     return (
         <ul className="col-md-4 list-grouip">
         {props.videos.length}
+        {videoItems}
         </ul>
     );
 };
